5f34aa4d0669346817ab2e20cf24c06db766c836
Rohit Pol <70880354+Rohit0911@users.noreply.github.com>
2024-06-17 14:00:29 +0000

    variables

---
+++ Basics/variables.js
@@ -0,0 +1,35 @@
+const roll=12334
+let accemail="rohitpol@google.com"
+var password="49384"
+city="Bengaluru"
+
+/** 
+ console.log(roll);
+console.log(accemail);
+console.log(password);
+console.log((city));
+console.log(typeof(city));
+*/
+
+
+
+// roll=12335
+accemail="rohitpol0911@gmail.com"
+password="9876"
+city="Pune"
+
+/*console.log(roll);
+console.log(accemail);
+console.log(password);
+console.log((city));*/
+
+//1. Except the constant variables all other types of variables are mutable
+//2. Another way to print the variables is
+
+
+console.table([roll,accemail,password,city])
+
+/*
+Dont use Var for declaring variables from now on and use Let.
+Var creates serious issues with the block scope.
+*/
